858c6d3bf0fa48081ecb30f40bfdd37c685188fc
Alipova <alipova09022003@gmail.com>
2023-03-26 18:49:43 +0600

    added comment

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import router from "./utils/routes";
 
 import "./global-styles/index.css";
 
+// This is we.
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     // <React.StrictMode>
