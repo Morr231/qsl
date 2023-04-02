d63230bfb82def3d851c620280d07b75e5903007
RamboXD <rnazymxan@gmail.com>
2023-04-02 18:22:41 +0600

    categories end

--- client/src/modules/CategoryPage/Category/Category.js
+++ client/src/modules/CategoryPage/Category/Category.js
@@ -5,7 +5,11 @@ import "./Category.sass";
 
 const Category = ({ category }) => {
   return (
-    <Link to={`categories/${category.name}`} state={category}>
+    <Link
+      to={`categories/${category.name}`}
+      state={category}
+      style={{ textDecoration: "none" }}
+    >
       <div className="category">
         <div className="category__img__container">
           <img
@@ -14,15 +18,52 @@ const Category = ({ category }) => {
             alt={category.svg}
           />
         </div>
-        <div className="category__desciption">
-          <div className="category__level">Оңай</div>
-          <div className="category__amount">25 сөз</div>
+        <div
+          className="category__desciption"
+          style={{
+            width: "200px",
+            marginBottom: "10px",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <div
+            className="category__level"
+            style={{
+              marginRight: "20px",
+              height: "20px",
+              width: "40px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#F1F1FD",
+              color: "#7879F1",
+              borderRadius: "30%",
+            }}
+          >
+            Оңай
+          </div>
+          <div
+            className="category__amount"
+            style={{
+              height: "20px",
+              width: "40px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            25 сөз
+          </div>
         </div>
         <div className="category__name">{category.name}</div>
         <div className="category__progress">
           Прогресс: 50%
           <div className="category__progress__line">
-            <div className="category__progress__line__filled"></div>
+            <div
+              style={{ width: "50%" }}
+              className="category__progress__line__filled"
+            ></div>
           </div>
         </div>
       </div>
